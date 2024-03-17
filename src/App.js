75729ff3bdcb83411bcf0e912ffe9b28d81cbc5a
SanOP360 <sanjayl2001alm@gmail.com>
2024-03-17 22:19:19 +0530

    created a frontend of Login Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+
+import { Route,Routes } from "react-router-dom";
+import SignUpForm from "./Pages/SignUpForm";
 import LoginForm from "./Pages/LoginForm";
+
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm></LoginForm>
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginForm/>} />
+      <Route path="/Signup" element={<SignUpForm />} />
+    </Routes>
   );
 }
 
--- src/Pages/SignUpForm.js
+++ src/Pages/SignUpForm.js
@@ -3,33 +3,43 @@ import axios from "axios";
 import "./loginStyles.css";
 import { Link } from "react-router-dom";
 
-
-const LoginForm = () => {
+const SignUpForm = () => {
+  const nameRef = useRef(null);
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
 
   const submitForm = (event) => {
     event.preventDefault();
+    const name = nameRef.current.value;
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
 
     axios
-      .post("http://localhost:3000/users/login", {email, password })
+      .post("http://localhost:3000/users/signup", { name, email, password })
       .then((response) => {
         console.log(response.data);
+        
       })
       .catch((error) => {
         console.error("Error signing up:", error);
       });
 
-    emailRef.current.value = null;
-    passwordRef.current.value = null;
+      nameRef.current.value=null;
+      emailRef.current.value=null;
+      passwordRef.current.value=null;
   };
 
   return (
     <div className="MainDiv">
       <form onSubmit={submitForm} className="custom-form">
-        <h1 className="heading">SIGN IN</h1>
+        <h1 className="heading">SIGN UP</h1>
+
+        <div className="sub-form">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input type="text" name="name" ref={nameRef} className="form-input" />
+        </div>
 
         <div className="sub-form">
           <label htmlFor="email" className="form-label">
@@ -57,16 +67,18 @@ const LoginForm = () => {
 
         <div className="formBtn">
           <button type="submit" className="custom-button">
-            Login
+            Submit
           </button>
         </div>
 
         <div className="divLink">
-            <Link className="link" to="/Signup">New-User ? Create a New Account </Link>
+          <Link className="link" to="/">
+            Already have an account? Login from here
+          </Link>
         </div>
       </form>
     </div>
   );
 };
 
-export default LoginForm;
+export default SignUpForm;
