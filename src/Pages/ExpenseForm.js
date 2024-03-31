3d4bb3bfe86018edb1bf80e2e135b1f4ecf3e827
SanOP360 <sanjayl2001alm@gmail.com>
2024-03-31 20:09:39 +0530

    added Leadership list

--- src/Pages/ExpenseForm.js
+++ src/Pages/ExpenseForm.js
@@ -4,9 +4,39 @@ import { useEffect, useRef, useState } from "react";
 
 const ExpenseForm = () => {
   const [expenses, setExpenses] = useState([]);
+  const [visibility,setVisibility]=useState(true);
+  const [leaderboard,setLeaderboard]=useState([]);
   const priceRef = useRef(null);
   const descriptionRef = useRef(null);
   const categoryRef = useRef(null);
+
+
+  useEffect(() => {
+    
+    const checkPremium = async () => {
+      try {
+        const token = localStorage.getItem("jwttoken");
+        const response = await axios.get(
+          "http://localhost:5000/premium/checkPremium",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        
+        if (response.data) {
+          setVisibility(false);
+        }
+      } catch (error) {
+        console.error(error);
+        
+      }
+    };
+
+    checkPremium(); 
+  }, []);
 
   const reloadExpenses = () => {
     const token = localStorage.getItem("jwttoken");
@@ -40,7 +70,7 @@ const ExpenseForm = () => {
     const description = descriptionRef.current.value;
     const category = categoryRef.current.value;
 
-    axios
+    axios 
       .post(
         "http://localhost:5000/Expense/add-expense",
         {
@@ -111,10 +141,9 @@ const ExpenseForm = () => {
             );
             console.log(successResponse);
             if (successResponse.data.success) {
-              alert("Payment successful & you are premium user");
-              console.log(successResponse);
+              alert("Payment successful & you are a premium user");
               localStorage.setItem("premium", true);
-              document.getElementById("premium-button").remove();
+              setVisibility(false);
             } else {
               alert("Payment failed");
             }
@@ -153,70 +182,128 @@ const ExpenseForm = () => {
       e.preventDefault();
     } catch (error) {
       console.error(error);
-      alert("Internal Server Error: " + error.response.data.msg);
+      alert("Internal Server Error: " + error);
     }
   };
 
 
-  return (
-    <div className="expense-form">
-      <button className="buyPrem" onClick={buyPremiumHandler}>
-        Buy Premium
-      </button>
-      <h1>Add Your Expenses here</h1>
-      <form onSubmit={submitExpenses}>
-        <div className="subform">
-          <label htmlFor="price">Price</label>
-          <input
-            type="number"
-            ref={priceRef}
-            name="price"
-            id="price"
-            className="input-field"
-          />
-        </div>
-        <div className="subform">
-          <label htmlFor="description">Description of the Expense</label>
-          <input
-            type="text"
-            ref={descriptionRef}
-            name="description"
-            id="description"
-            className="input-field"
-          />
-        </div>
-        <div className="subform">
-          <label htmlFor="categories">Category</label>
-          <select
-            ref={categoryRef}
-            name="categories"
-            id="categories"
-            className="select-field"
-          >
-            <option value="food">Food</option>
-            <option value="travel">Travel</option>
-            <option value="rent">Rent</option>
-            <option value="loan">Loan</option>
-          </select>
-        </div>
-        <button type="submit" className="expense-btn">
-          Done
-        </button>
-      </form>
+ const showLeaderBoardHandler = async () => {
+  try {
+    const token = localStorage.getItem("jwttoken");
 
-      <div className="expense-list">
-        <h2>Expense List</h2>
-        {expenses.map((expense) => (
-          <div key={expense.id} className="expense-item">
-            <p>Price: ${expense.price}</p>
-            <p>Description: {expense.description}</p>
-            <p>Category: {expense.categories}</p>
-            <button onClick={() => deleteExpense(expense.id)}>Delete</button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    const response = await axios.get("http://localhost:5000/premium/showleaderboard", {
+      headers: {
+        Authorization: token
+      }
+    });
+
+    const leaderboard = response.data.outputArr;
+
+    leaderboard.sort((a, b) => b.totalSpent - a.totalSpent);
+
+    setLeaderboard(leaderboard);
+
+    leaderboard.forEach((entry) => {
+      console.log(`${entry.name}: Expense ${entry.totalSpent}`);
+
+    });
+
+    
+  } catch (error) {
+    console.error(error);
+    
+  }
+};
+
+
+  
+
+ return (
+   <div className="expense-form">
+     {visibility && (
+       <button
+         className="buyPrem"
+         id="premium-button"
+         onClick={buyPremiumHandler}
+       >
+         Buy Premium
+       </button>
+     )} 
+     {!visibility &&
+       <>
+         <p>You are a premium user</p>
+         <button className="leaderboard-btn" onClick={showLeaderBoardHandler}>
+           Show LeaderBoard
+         </button>
+       </>
+     }
+
+     <h1>Add Your Expenses here</h1>
+     <form onSubmit={submitExpenses}>
+       <div className="subform">
+         <label htmlFor="price">Price</label>
+         <input
+           type="number"
+           ref={priceRef}
+           name="price"
+           id="price"
+           className="input-field"
+         />
+       </div>
+       <div className="subform">
+         <label htmlFor="description">Description of the Expense</label>
+         <input
+           type="text"
+           ref={descriptionRef}
+           name="description"
+           id="description"
+           className="input-field"
+         />
+       </div>
+       <div className="subform">
+         <label htmlFor="categories">Category</label>
+         <select
+           ref={categoryRef}
+           name="categories"
+           id="categories"
+           className="select-field"
+         >
+           <option value="food">Food</option>
+           <option value="travel">Travel</option>
+           <option value="rent">Rent</option>
+           <option value="loan">Loan</option>
+         </select>
+       </div>
+       <button type="submit" className="expense-btn">
+         Done
+       </button>
+     </form>
+
+     <div className="expense-list">
+       <h2>Expense List</h2>
+       {expenses.map((expense) => (
+         <div key={expense.id} className="expense-item">
+           <p>Price: ${expense.price}</p>
+           <p>Description: {expense.description}</p>
+           <p>Category: {expense.categories}</p>
+           <button onClick={() => deleteExpense(expense.id)}>Delete</button>
+         </div>
+       ))}
+     </div>
+
+     <div className="leaderboard-container">
+       {visibility && <h2>Leaderboard</h2>}
+       <div className="leaderboard">
+         {leaderboard.map((entry, index) => (
+           <div key={index} className="leaderboard-entry">
+             <p>{`${entry.name}: Expense ${entry.totalSpent}`}</p>
+           </div>
+         ))}
+       </div>
+     </div>
+   </div>
+ );
+
 };
 
 export default ExpenseForm;
